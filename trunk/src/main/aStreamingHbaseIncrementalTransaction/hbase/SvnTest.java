c6b9c1d59a2e367d6468fba471c3bc41babb433b
wliu.hust@gmail.com <wliu.hust@gmail.com@e5459917-d826-9f31-aafb-381521d7435f>
2012-05-03 01:53:49 +0000

    author: wliu description: Just for test

--- trunk/src/main/aStreamingHbaseIncrementalTransaction/hbase/SvnTest.java
+++ trunk/src/main/aStreamingHbaseIncrementalTransaction/hbase/SvnTest.java
@@ -3,5 +3,7 @@ package main.aStreamingHbaseIncrementalTransaction.hbase;
 public class SvnTest {
 	public SvnTest()	{}
 	
-	public static void main(String[] args)	{}
+	public static void main(String[] args)	{
+		System.out.println("Test");
+	}
 }
