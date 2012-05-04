157d50c47dc50b29199994ceb8a185c67e2c3798
hyczzhu@gmail.com <hyczzhu@gmail.com@e5459917-d826-9f31-aafb-381521d7435f>
2012-05-04 13:15:27 +0000

    a simple timestamp generator

---
+++ trunk/src/main/java/aStreamingHbaseIncrementalTransaction/DungbeeMain.java
@@ -0,0 +1,8 @@
+package aStreamingHbaseIncrementalTransaction;
+
+public class DungbeeMain {
+
+	public static void main(String[] args){
+		System.out.println("This is Dungbee Helloworld");
+	}
+}
---
+++ trunk/src/main/java/aStreamingHbaseIncrementalTransaction/hbase/SvnTest.java
@@ -0,0 +1,9 @@
+package aStreamingHbaseIncrementalTransaction.hbase;
+
+public class SvnTest {
+	public SvnTest()	{}
+	
+	public static void main(String[] args)	{
+		System.out.println("Test");
+	}
+}
