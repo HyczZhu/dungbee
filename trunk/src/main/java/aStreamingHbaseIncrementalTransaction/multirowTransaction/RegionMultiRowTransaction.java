8478053a85faff4b5164a0a879835e96fe0f5640
hyczzhu@gmail.com <hyczzhu@gmail.com@e5459917-d826-9f31-aafb-381521d7435f>
2012-05-12 09:52:58 +0000

    Methods that lock a row to put and get data, as well as some test code

---
+++ trunk/src/main/java/aStreamingHbaseIncrementalTransaction/multirowTransaction/RegionMultiRowTransaction.java
@@ -0,0 +1,5 @@
+package aStreamingHbaseIncrementalTransaction.multirowTransaction;
+
+public class RegionMultiRowTransaction {
+
+}
---
+++ trunk/src/main/java/aStreamingHbaseIncrementalTransaction/multirowTransaction/TestRowTransaction.java
@@ -0,0 +1,82 @@
+package aStreamingHbaseIncrementalTransaction.multirowTransaction;
+
+import java.io.IOException;
+
+import aStreamingHbaseIncrementalTransaction.example.TestHBaseBasicClient;
+
+public class TestRowTransaction extends RowTransaction{
+	private static final String TABLE_NAME = "SimpleDungbeeTest";
+	private static final String TABLE_NAME_2 = "SimpleDungbeeTest2";
+	private static final String COLUMN_FAMILY = "SimpleColumnFamily";
+	
+	public static int threadcount = 0;
+
+
+	/***
+	 * use multi thread to lock and put data into a row.
+	 * using some default put parameters
+	 * @param threadNum
+	 */
+	public void multiThreadLockAndPut(int threadNum) {
+		multiThreadLockAndPut(threadNum, TABLE_NAME,
+				"row_multiThreadLockAndPut", COLUMN_FAMILY, "column",
+				"multiThreadLockAndPut");
+	}
+
+	/***
+	 * use multi thread to lock and put data into a row
+	 * @param threadNum
+	 * @param tableName
+	 * @param rowName
+	 * @param familyName
+	 * @param columnName
+	 * @param value
+	 */
+	public void multiThreadLockAndPut(int threadNum, final String tableName,
+			final String rowName, final String familyName,
+			final String columnName, final String value) {
+		for (int i = 0; i < threadNum; i++) {
+			final int tnum = i;
+			Thread t = new Thread(new Runnable() {
+				public void run() {
+					TestRowTransaction instance = new TestRowTransaction();
+					try {
+						instance.lockAndPut(tableName, rowName, familyName,
+								columnName + tnum, value + tnum);
+					} catch (IOException e) {
+						e.printStackTrace();
+					} finally {
+						TestRowTransaction.threadcount++;
+					}
+				}
+			});
+			t.start();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		try {
+			TestRowTransaction trt = new TestRowTransaction();
+			// for (int i = 0; i < 1; i++) {
+			// new TestRowTransaction().lockAndPut(TABLE_NAME, "row" + i,
+			// COLUMN_FAMILY, "column",
+			// "lockAndPut-value" + i);
+			// TestHBaseBasicClient.queryFamily(TABLE_NAME, "row" + i,
+			// COLUMN_FAMILY);
+			// }
+			// TestHBaseBasicClient.queryTable(TABLE_NAME);
+			int threadnum = 4;
+			trt.multiThreadLockAndPut(threadnum);
+			while (true) {
+				if (TestRowTransaction.threadcount == threadnum) {
+					TestHBaseBasicClient.queryRow(TABLE_NAME,
+							"row_multiThreadLockAndPut");
+					break;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
