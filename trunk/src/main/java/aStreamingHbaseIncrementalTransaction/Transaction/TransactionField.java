0edad823c6a1804bd63d2ed4a0422528ce31e4f7
wliu.hust@gmail.com <wliu.hust@gmail.com@e5459917-d826-9f31-aafb-381521d7435f>
2012-05-16 14:21:22 +0000

    删除Transaction包（改为了transaction）

--- trunk/src/main/java/aStreamingHbaseIncrementalTransaction/Transaction/TransactionField.java
+++
@@ -1,9 +0,0 @@
-package aStreamingHbaseIncrementalTransaction.Transaction;
-
-public class TransactionField {
-	public static final byte[] _DATA = "_data".getBytes();
-	public static final byte[] _WRITE = "_write".getBytes();
-	public static final byte[] _LOCK = "_lock".getBytes();
-	public static final byte[] _NOTIFY = "_notify".getBytes();
-	public static final byte[] _ACK_O = "_ack_o".getBytes();
-}
